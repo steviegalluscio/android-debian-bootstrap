b939990988e87f72eb3a84d8a3c6b47013f4f221
steviegalluscio <galluscio.stevie@gmail.com>
2024-01-27 15:40:00 -0500

    Making /etc/resolv.conf immutable

--- scripts/install-bootstrap.sh
+++ scripts/install-bootstrap.sh
@@ -8,8 +8,11 @@ cd bootstrap
 cat ../rootfs.tar.xz | ../root/bin/minitar
 cd ..
 
-# include resolv.conf
+# create resolv.conf
+rm -f bootstrap/etc/resolv.conf
 echo "nameserver 8.8.8.8 \n \
 nameserver 8.8.4.4" > bootstrap/etc/resolv.conf
+# make resolv.conf immutable
+chattr +i bootstrap/etc/resolv.conf
 
 echo "bootstrap ready, run with run-bootstrap.sh"
